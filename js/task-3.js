c7ba357413eae1f74cb770e0b852b9aff7142359
Mariya Stepa <stepa@DESKTOP-KAIIJ29>
2025-03-07 14:54:53 +0100

    3. task edit

--- js/task-3.js
+++ js/task-3.js
@@ -4,9 +4,8 @@
 
 // Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
 const sortByDescendingFriendCount = (users) => {
-  return users.slice().sort((a, b) => b.friends.length - a.friends.length);
+  return users.toSorted((a, b) => b.friends.length - a.friends.length);
 };
-
 
 console.log(
   sortByDescendingFriendCount([
@@ -47,40 +46,4 @@ console.log(
     }
   ])
 );
-// [
-//   {
-//     name: "Ross Vazquez",
-//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Sharlene Bush",
-//     friends: ["Briana Decker", "Sharron Pace"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Elma Head",
-//     friends: ["Goldie Gentry", "Aisha Tran"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Carey Barr",
-//     friends: ["Jordan Sampson", "Eddie Strong"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Blackburn Dotson",
-//     friends: ["Jacklyn Lucas", "Linda Chapman"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Moore Hensley",
-//     friends: ["Sharron Pace"],
-//     gender: "male"
-//   }
-// ]
+// Результат буде відсортований за кількістю друзів в порядку спадання
